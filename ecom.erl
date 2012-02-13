cf4fc0507417c84da3b3e827f0df83a0a386e7af
comptekki <comptekki@gmail.com>
2012-02-13 12:41:40 -0700

    change ninitelog command name

--- ecom.erl
+++ ecom.erl
@@ -84,7 +84,7 @@ process_msg(Box, Com, Args, Msg_PID) ->
 					{Year,Month,Day}=date(),
 					Date=lists:flatten(io_lib:format("~p~2..0B~2..0B",[Year,Month,Day])),
 					os:cmd("c:/erl/uploads/NiniteOne.exe /updateonly /exclude Python /silent "++?UPLOADS_DIR++"\\"++Date++"log.txt");
-				"ninitemlog" ->
+				"ninitelog" ->
 					{Year,Month,Day}=date(),
 					Date=lists:flatten(io_lib:format("~p~2..0B~2..0B",[Year,Month,Day])),
 					{ok, Log}=file:read_file(?UPLOADS_DIR++"\\"++Date++"log.txt"),
